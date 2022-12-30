d777a02fefa514c8e5e6653d1df7be55b437d72c
“BIGboss248” <aminjamali79@gmail.com>
2022-12-30 20:37:14 +0330

    initial setup

---
+++ Programing.cmd
@@ -0,0 +1,8 @@
+choco install vscode --version 1.74.2 -y
+choco install git --version 2.39.0 -y
+choco install python3 --version 3.11.0 -y
+choco install docker-desktop --version 4.15.0 -y
+choco install virtualbox --version 7.0.4 -y
+choco install chocolateygui --version 1.1.0 -y
+choco install 7zip.install --version 22.1 -y
+choco install pgadmin4 --version 6.13 -y
