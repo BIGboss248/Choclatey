9dc13c032065952a446ad3d821b78653ea176e88
BIGboss248 <aminjamali79@gmail.com>
2022-12-31 13:30:05 +0330

    added proxy settings to be disabled when downloading by winget

--- Office.cmd
+++ Office.cmd
@@ -5,7 +5,9 @@ choco install firefox --version 108.0.1 -y
 choco install 7zip.install --version 22.1 -y
 choco install vlc --version 3.0.18 -y
 choco install chocolateygui --version 1.1.0 -y
+choco install puppet-agent -y
+Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\' -Name 'ProxyEnable' -Value 0
 winget install --name "HP Smart" --accept-package-agreements --accept-source-agreements
 winget install --name "notepad++" --accept-package-agreements --accept-source-agreements
-winget install --name "python 3.11" --accept-package-agreements --accept-source-agreements
 winget install --name "Windows Terminal" --accept-package-agreements --accept-source-agreements
+winget install --name "python 3.11" --accept-package-agreements --accept-source-agreements
--- Programing.cmd
+++ Programing.cmd
@@ -1,11 +1,14 @@
-choco install vscode --version 1.74.2 -y
 choco install git --version 2.39.0 -y
 choco install docker-desktop --version 4.15.0 -y
 choco install virtualbox --version 7.0.4 -y
 choco install chocolateygui --version 1.1.0 -y
 choco install 7zip.install --version 22.1 -y
 choco install pgadmin4 --version 6.13 -y
+choco install spotify -y
+Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\' -Name 'ProxyEnable' -Value 0
+winget install --name "Windows Terminal" --accept-package-agreements --accept-source-agreements
 winget install --name "notepad++" --accept-package-agreements --accept-source-agreements
 winget install --name "python 3.11" --accept-package-agreements --accept-source-agreements
+winget install --name "Visual Studio Code" --accept-package-agreements --accept-source-agreements
 winget install --name "Power BI Desktop" --accept-package-agreements --accept-source-agreements
-winget install --name "Windows Terminal" --accept-package-agreements --accept-source-agreements
+winget install --name "Ubuntu 22.04.1 LTS" --accept-package-agreements --accept-source-agreements
