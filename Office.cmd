8513f3f3f7f7c819b06b04e8017f15ae2d21aabf
BIGboss248 <aminjamali79@gmail.com>
2022-12-31 12:51:27 +0330

    Programs listed and added both with winget and chocolatey

---
+++ Office.cmd
@@ -0,0 +1,11 @@
+choco install foxitreader --version 12.1.0.15250 -y
+choco install anydesk --version 7.1.7 -y
+choco install googlechrome --version 108.0.5359.125 -y
+choco install firefox --version 108.0.1 -y
+choco install 7zip.install --version 22.1 -y
+choco install vlc --version 3.0.18 -y
+choco install chocolateygui --version 1.1.0 -y
+winget install --name "HP Smart" --accept-package-agreements --accept-source-agreements
+winget install --name "notepad++" --accept-package-agreements --accept-source-agreements
+winget install --name "python 3.11" --accept-package-agreements --accept-source-agreements
+winget install --name "Windows Terminal" --accept-package-agreements --accept-source-agreements
--- Programing.cmd
+++ Programing.cmd
@@ -1,8 +1,11 @@
 choco install vscode --version 1.74.2 -y
 choco install git --version 2.39.0 -y
-choco install python3 --version 3.11.0 -y
 choco install docker-desktop --version 4.15.0 -y
 choco install virtualbox --version 7.0.4 -y
 choco install chocolateygui --version 1.1.0 -y
 choco install 7zip.install --version 22.1 -y
 choco install pgadmin4 --version 6.13 -y
+winget install --name "notepad++" --accept-package-agreements --accept-source-agreements
+winget install --name "python 3.11" --accept-package-agreements --accept-source-agreements
+winget install --name "Power BI Desktop" --accept-package-agreements --accept-source-agreements
+winget install --name "Windows Terminal" --accept-package-agreements --accept-source-agreements
