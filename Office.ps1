b9db0b2c9e75599a5686efcc9fa638204990fae8
BIGboss248 <aminjamali79@gmail.com>
2022-12-31 17:55:46 +0330

    Office packages are all chocolatey now to internlize

--- Office.ps1
+++ Office.ps1
@@ -5,8 +5,6 @@ choco install firefox -y
 choco install 7zip.install -y
 choco install vlc -y
 choco install chocolateygui -y
-Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\' -Name 'ProxyEnable' -Value 0
-winget install --name "HP Smart" --accept-package-agreements --accept-source-agreements
-winget install --name "notepad++" --accept-package-agreements --accept-source-agreements
-winget install --name "Windows Terminal" --accept-package-agreements --accept-source-agreements
-winget install --name "python 3.11" --accept-package-agreements --accept-source-agreements
+choco install notepadplusplus.install -y
+choco install python -y
+choco install microsoft-windows-terminal -y
