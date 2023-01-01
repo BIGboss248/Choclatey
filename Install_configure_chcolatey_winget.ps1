23e8270c2a11e2a44d6749d43d76d05e8cfab48f
BIGboss248 <aminjamali79@gmail.com>
2023-01-01 13:21:50 +0330

    choco automatic installation added

--- Install_configure_chcolatey_winget.ps1
+++ Install_configure_chcolatey_winget.ps1
@@ -2,4 +2,5 @@ set-executionpolicy remotesigned
 # Copy the items of Local choco setup to drive c
 $Path = Join-Path -Path $pwd  -ChildPath ".\Choclatey\Resources\choco-setup"
 Copy-Item $Path "C:\" -recurse
-# cd "C:\choco-setup\files"
+$choco_path = "C:\choco-setup\files\ChocolateyLocalInstall.ps1"
+Install-Script $choco_path
