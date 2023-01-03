c1ada969728ec4cd8a4528e0ffed5936676d2dd7
BIGboss248 <aminjamali79@gmail.com>
2023-01-03 12:04:55 +0330

    wget installer and puppet bolt added

--- Programing.ps1
+++ Programing.ps1
@@ -5,6 +5,7 @@ choco install chocolateygui -y
 choco install 7zip.install -y
 choco install pgadmin4 -y
 choco install spotify -y
+choco install puppet-bolt -y
 Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\' -Name 'ProxyEnable' -Value 0
 winget install --name "PuTTY" --accept-package-agreements --accept-source-agreements
 winget install --name "Windows Terminal" --accept-package-agreements --accept-source-agreements
