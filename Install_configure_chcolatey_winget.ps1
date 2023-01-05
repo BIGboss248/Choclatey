04627f5b1f0267602496585476dc03c20496e7dc
BIGboss248 <aminjamali79@gmail.com>
2023-01-05 15:12:30 +0330

    execution policy seprateed and install admins itself

--- Install_configure_chcolatey_winget.ps1
+++ Install_configure_chcolatey_winget.ps1
@@ -1,4 +1,20 @@
-set-executionpolicy remotesigned
+param([switch]$Elevated)
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
+
+'running with full privileges'
 # Copy the items of Local choco setup to drive c
 $Path = Join-Path -Path $pwd  -ChildPath ".\Choclatey\Resources\choco-setup"
 Copy-Item $Path "C:\" -recurse
