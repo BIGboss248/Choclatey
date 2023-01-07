b2743ebd743d71fe016ff371f72e019029efb7bf
BIGboss248 <aminjamali79@gmail.com>
2023-01-07 13:37:34 +0330

    execution policy changed to bypass

--- Install_configure_chcolatey_winget.ps1
+++ Install_configure_chcolatey_winget.ps1
@@ -1,15 +1,16 @@
-$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$script_Path = split-path -parent $MyInvocation.MyCommand.Path
+$Choco_Path_dir = Join-Path -Path $script_Path  -ChildPath "\Resources\choco-setup"
+$nuget_path = Join-Path -path $script_Path -ChildPath "\Resources\Microsoft.PackageManagement.NuGetProvider-2.8.5.208.dll"
+$choco_path = "C:\choco-setup\files\ChocolateyLocalInstall.ps1"
+
 'running with full privileges'
-set-executionpolicy unrestricted
+Set-ExecutionPolicy Bypass -Scope Process -Force;
 # Copy the items of Local choco setup to drive c
-Set-Location $scriptPath
-$Path = Join-Path -Path $scriptPath  -ChildPath "\Resources\choco-setup"
-$nuget_path = Join-Path -path $scriptPath -ChildPath "\Resources\Microsoft.PackageManagement.NuGetProvider-2.8.5.208.dll"
-Read-Host -Prompt "Press Enter to exit"
-Copy-Item $Path "C:\" -recurse
+Set-Location $script_Path
+Copy-Item $Choco_Path_dir "C:\" -recurse
+#Install Nuget Package Provider
 mkdir "C:\Program Files\PackageManagement\ProviderAssemblies\nuget\2.8.5.208"
 Copy-Item -Path $nuget_path -Destination "C:\Program Files\PackageManagement\ProviderAssemblies\nuget\2.8.5.208"
-$choco_path = "C:\choco-setup\files\ChocolateyLocalInstall.ps1"
+#Install Chocolatey
 Set-Location $choco_path
-Read-Host -Prompt "Press Enter to exit"
 Start-Process powershell -verb runas -ArgumentList $choco_path
--- Setup.ps1
+++
@@ -1,3 +0,0 @@
-$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
-Set-Location $scriptPath
-Start-Process powershell -verb runas -ArgumentList -file Install_configure_chcolatey_winget.ps1
