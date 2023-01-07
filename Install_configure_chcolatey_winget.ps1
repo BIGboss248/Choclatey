f1adc411be7cfad09f6e7399691e2d3b8405f735
BIGboss248 <aminjamali79@gmail.com>
2023-01-07 12:11:43 +0330

    added nuget dll and admin priviliges is now given by setup.ps1

--- Install_configure_chcolatey_winget.ps1
+++ Install_configure_chcolatey_winget.ps1
@@ -1,23 +1,15 @@
-param([switch]$Elevated)
-
-function Test-Admin {
-    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-}
-
-if ((Test-Admin) -eq $false)  {
-    if ($elevated) {
-        # tried to elevate, did not work, aborting
-    } else {
-        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
-    }
-    exit
-}
-
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 'running with full privileges'
 set-executionpolicy unrestricted
 # Copy the items of Local choco setup to drive c
-$Path = Join-Path -Path $pwd  -ChildPath ".\Choclatey\Resources\choco-setup"
+Set-Location $scriptPath
+$Path = Join-Path -Path $scriptPath  -ChildPath "\Resources\choco-setup"
+$nuget_path = Join-Path -path $scriptPath -ChildPath "\Resources\Microsoft.PackageManagement.NuGetProvider-2.8.5.208.dll"
+Read-Host -Prompt "Press Enter to exit"
 Copy-Item $Path "C:\" -recurse
+mkdir "C:\Program Files\PackageManagement\ProviderAssemblies\nuget\2.8.5.208"
+Copy-Item -Path $nuget_path -Destination "C:\Program Files\PackageManagement\ProviderAssemblies\nuget\2.8.5.208"
 $choco_path = "C:\choco-setup\files\ChocolateyLocalInstall.ps1"
-Install-Script $choco_path
+Set-Location $choco_path
+Read-Host -Prompt "Press Enter to exit"
+Start-Process powershell -verb runas -ArgumentList $choco_path
