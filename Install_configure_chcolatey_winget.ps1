81112dc3327b11b4672d26fbd95a4705fa8582d7
BIGboss248 <aminjamali79@gmail.com>
2023-01-07 09:26:37 +0330

    execution policy in install script

--- Install_configure_chcolatey_winget.ps1
+++ Install_configure_chcolatey_winget.ps1
@@ -15,6 +15,7 @@ if ((Test-Admin) -eq $false)  {
 }
 
 'running with full privileges'
+set-executionpolicy unrestricted
 # Copy the items of Local choco setup to drive c
 $Path = Join-Path -Path $pwd  -ChildPath ".\Choclatey\Resources\choco-setup"
 Copy-Item $Path "C:\" -recurse
