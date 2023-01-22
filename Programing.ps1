6fc9ff99e70e275946a3742d93db91ab8319c262
BIGboss248 <aminjamali79@gmail.com>
2023-01-22 17:14:12 +0330

    added termius instead of putty

--- Programing.ps1
+++ Programing.ps1
@@ -1,23 +1,25 @@
-choco install git -y
 choco install docker-desktop -y
 choco install virtualbox -y
 choco install chocolateygui -y
 choco install 7zip.install -y
 choco install pgadmin4 -y
 choco install spotify -y
-choco install ruby -y
+#backup
 choco install dropbox -y
 choco install googledrive -y
 choco install chef-workstation -y
-choco install putty.install -y
+choco install mremoteng -y
 choco install microsoft-windows-terminal -y
 choco install notepadplusplus.install -y
+choco install git -y
+choco install ruby -y
 choco install python -y
 choco install vscode.install -y
+# Browsers
 choco install opera-gx -y
 choco install firefox -y
 choco install googlechrome -y
-choco install winsetupfromusb -y
+choco install termius -y
 #Possible malware in this package
 choco install winsetupfromusb -y
 Install-Module ps2exe -Force
