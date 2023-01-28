a846b86aedacf56527fa8dd5a415ed28765cecef
BIGboss248 <aminjamali79@gmail.com>
2023-01-28 12:43:41 +0330

    catagorized and modified the packages

--- Programing.ps1
+++ Programing.ps1
@@ -1,25 +1,33 @@
-choco install docker-desktop -y
-choco install virtualbox -y
+# chocolatey config file C:\ProgramData\chocolatey\config\chocolatey.config
+
+#initialize
 choco install chocolateygui -y
 choco install 7zip.install -y
-choco install pgadmin4 -y
-choco install spotify -y
+choco install anydesk.in
+# virtual OS
+choco install docker-desktop -y
+choco install virtualbox -y
 #backup
 choco install dropbox -y
 choco install googledrive -y
-choco install chef-workstation -y
-choco install mremoteng -y
-choco install microsoft-windows-terminal -y
+#coding
 choco install notepadplusplus.install -y
+choco install microsoft-windows-terminal -y
 choco install git -y
 choco install ruby -y
 choco install python -y
 choco install vscode.install -y
+choco install pgadmin4 -y
 # Browsers
 choco install opera-gx -y
 choco install firefox -y
 choco install googlechrome -y
 choco install termius -y
+#video player
+choco install vlc -y
+# Game
+choco install steam -y
+choco install origin -y
 #Possible malware in this package
 choco install winsetupfromusb -y
 Install-Module ps2exe -Force
