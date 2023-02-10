5bb334382ca681e4db9642e52d30f8a053ad7572
BIGboss248 <aminjamali79@gmail.com>
2023-02-10 20:54:37 +0330

    Configured dns to bypass embargos

--- Shecan_DNS.ps1
+++ Shecan_DNS.ps1
@@ -5,9 +5,14 @@
 #   google      8.8.8.8     8.8.4.4
 #   Cisco       208.67.222.222  208.67.220.220
 #   Iran        5.200.200.200
+#   SSRR        114.114.114.114:53  223.5.5.5:53    208.67.222.222:53
+
+
+$nic = Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object {$_.IPEnabled -eq "TRUE"}
+$nic.SetDNSServerSearchOrder($null)
 
 # Define DNS Servers
-$DNSServers = @("10.202.10.10","178.22.122.100","1.1.1.1")
+$DNSServers = @("178.22.122.100","185.51.200.2")
 
 # Get Network Adapters with IP Address enabled
 $nic = Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object {$_.IPEnabled -eq "TRUE"}
