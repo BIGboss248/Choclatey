6e2d2355e1a7780e23addb65536caf21953764d4
BIGBOSS248 <aminjamali79@gmail.com>
2023-12-11 16:07:27 +0330

    fixed typo

--- Programing-BTP-IT.ps1
+++ Programing-BTP-IT.ps1
@@ -2,19 +2,17 @@
 
 
 # Check for permissions 
-if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    # Relaunch as administrator
-    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
-    Start-Process powershell -Verb RunAs -ArgumentList $arguments
-    Exit
-}
-
+# if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+#     # Relaunch as administrator
+#     $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+#     Start-Process powershell -Verb RunAs -ArgumentList $arguments
+#     Exit
+# }
 '________________running with full privileges________________'
 Set-ExecutionPolicy Bypass -Scope Process -Force;
 '________________Disalbe right click file properties menue___________'
 reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope CurrentUser
-
 
 '________________Doc viewer and editer________________'
 winget install -e --id Foxit.FoxitReader --accept-source-agreements
@@ -22,9 +20,7 @@ winget install --id 9WZDNCRD29V9 --accept-source-agreements #install office365
 '________________Initialize and prerequisite________________'
 winget install --id Microsoft.DotNet.SDK.7 --accept-source-agreements
 winget install --id Microsoft.DotNet.Runtime.7 --accept-source-agreements
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 winget install --id Starship.Starship --accept-source-agreements
-Copy-Item "C:\Users\aminj\OneDrive\My Software\Choclatey\starship.toml" "C:\Users\aminj\.config"
 winget install --id 7zip.7zip --accept-source-agreements
 winget install -e --id AnyDeskSoftwareGmbH.AnyDesk --accept-source-agreements
 '________________virtual OS and Cloud________________'
@@ -34,7 +30,7 @@ winget install Canonical.Ubuntu.2204 --accept-source-agreements
 winget  install kalilinux.kalilinux --accept-source-agreements
 winget install -e --id Oracle.VirtualBox --accept-source-agreements
 winget install --id Hashicorp.Terraform --accept-source-agreements
-winget install --id Amazon.AWSCLI --accept-source-agreements
+winget install --id Amazon.AWSC1LI --accept-source-agreements
 '________________Coding________________'
 winget install --id 9NCVDN91XZQP --accept-source-agreements #Python3.12
 winget install -e --id XP9KHM4BK9FZ7Q --accept-source-agreements # VS code
@@ -43,7 +39,6 @@ winget install --id 9N0DX20HK701 --accept-source-agreements # Windwos terminal
 '________________Internet browsers________________' 
 winget install -e --id Waterfox.Waterfox --accept-source-agreements
 winget install -e --id Tonec.InternetDownloadManager --accept-source-agreements
-winget install --id OpenVPNTechnologies.OpenVPNConnect --accept-source-agreements
 '________________Video player________________'
 winget install -e --id VideoLAN.VLC --accept-source-agreements
 '________________Game________________'
