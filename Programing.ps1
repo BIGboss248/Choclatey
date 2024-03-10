923d80011ef98edb08c8103b29e4f91f251d5442
BIGBOSS248 <aminjamali79@gmail.com>
2024-03-10 13:11:28 +0330

    added wingetUI and an export of all my packages

--- Programing.ps1
+++ Programing.ps1
@@ -16,6 +16,8 @@ reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\Inpr
 Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope CurrentUser
 
 
+# Winget package manager
+winget install SomePythonThings.WingetUIStore
 '________________Doc viewer and editer________________'
 winget install -e --id Foxit.FoxitReader --accept-source-agreements
 winget install --id 9WZDNCRD29V9 --accept-source-agreements #install office365
@@ -30,6 +32,17 @@ winget install -e --id AnyDeskSoftwareGmbH.AnyDesk --accept-source-agreements
 '________________virtual OS and Cloud________________'
 # Install zsh and "oh my zsh" and power10k
 winget install -e --id Docker.DockerDesktop --accept-source-agreements
+winget install -e --id OpenJS.NodeJS --accept-source-agreements
+
+# Install mega linter an excelent tool for securing code and a clean code
+npm install mega-linter-runner -g
+
+# Install anaconda package manager
+winget install -e --id Anaconda.Anaconda3 --accept-source-agreements
+
+# Install precommit a tool used to lint a code before commiting it to github repository
+conda install -c conda-forge pre-commit
+
 winget install Canonical.Ubuntu.2204 --accept-source-agreements
 winget  install kalilinux.kalilinux --accept-source-agreements
 winget install -e --id Oracle.VirtualBox --accept-source-agreements
@@ -44,6 +57,7 @@ winget install --id 9N0DX20HK701 --accept-source-agreements # Windwos terminal
 winget install -e --id Waterfox.Waterfox --accept-source-agreements
 winget install -e --id Tonec.InternetDownloadManager --accept-source-agreements
 winget install --id OpenVPNTechnologies.OpenVPNConnect --accept-source-agreements
+winget install --id Famatech.AdvancedIPScanner --accept-source-agreements
 '________________Video player________________'
 winget install -e --id VideoLAN.VLC --accept-source-agreements
 '________________Game________________'
