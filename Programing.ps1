d401648c9598b7a6513560c06b78209b0e0a5486
BIGBOSS248 <aminjamali79@gmail.com>
2024-03-11 21:53:30 +0330

    linted and secured code

--- Programing.ps1
+++ Programing.ps1
@@ -1,22 +1,4 @@
-# chocolatey config file C:\ProgramData\chocolatey\config\chocolatey.config
-
-
-# Check for permissions
-if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    # Relaunch as administrator
-    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
-    Start-Process powershell -Verb RunAs -ArgumentList $arguments
-    Exit
-}
-
-'________________running with full privileges________________'
-Set-ExecutionPolicy Bypass -Scope Process -Force;
-'________________Disalbe right click file properties menue___________'
-reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
-Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope CurrentUser
-
-
-# Winget package manager
+﻿# Winget package manager
 winget install --id SomePythonThings.WingetUIStore --accept-source-agreements
 winget install --id ZeroTier.ZeroTierOne --accept-source-agreements
 '________________Doc viewer and editer________________'
@@ -25,7 +7,6 @@ winget install --id 9WZDNCRD29V9 --accept-source-agreements #install office365
 '________________Initialize and prerequisite________________'
 winget install --id Microsoft.DotNet.SDK.7 --accept-source-agreements
 winget install --id Microsoft.DotNet.Runtime.7 --accept-source-agreements
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 winget install --id Starship.Starship --accept-source-agreements
 Copy-Item "C:\Users\aminj\OneDrive\My Software\Choclatey\starship.toml" "C:\Users\aminj\.config"
 winget install --id 7zip.7zip --accept-source-agreements
